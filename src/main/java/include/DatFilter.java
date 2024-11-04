9667d0b47c605ea568004e8452e00ac594b5a9dd
João Pedro A.P.P. Barbosa <yabotz2222@gmail.com>
2024-11-04 20:53:24 -0300

    Adicionando o normalizador da língua

--- src/main/java/include/DatFilter.java
+++ src/main/java/include/DatFilter.java
@@ -11,75 +11,71 @@ import java.util.List;
 import java.util.Set;
 
 public class DatFilter {
-	
-	public static Set<String> PALAVRAS_IGNORADAS = new HashSet<>(Arrays.asList(
-        "a", "as", "ao", "aos", "até", "de", "da", "das", "do", "dos", "e", "em", "mas", "na", 
-	    "nas", "nem", "no", "nos", "o", "os", "ou", "para", "pelo", "pelos", "pois", "por", 
-	    "que", "se", "um", "uma", "uns", "umas"
-	));
-	
-	private String datFile;
-	private String palavrasChave;
 
-	public DatFilter(String datFile, String palavrasChave) {
-		super();
-		this.datFile = datFile;
-		this.palavrasChave = palavrasChave;
-		filtro(datFile, palavrasChave);
-	}
+    public static Set<String> PALAVRAS_IGNORADAS = new HashSet<>(Arrays.asList(
+            "a", "as", "ao", "aos", "até", "de", "da", "das", "do", "dos", "e", "em", "mas", "na",
+            "nas", "nem", "no", "nos", "o", "os", "ou", "para", "pelo", "pelos", "pois", "por",
+            "que", "se", "um", "uma", "uns", "umas"));
 
-	public DatFilter() {
-		super();
-	}
+    private String datFile;
+    private String palavrasChave;
 
-	public String getDatFile() {
-		return datFile;
-	}
+    public DatFilter(String datFile, String palavrasChave) {
+        super();
+        this.datFile = datFile;
+        this.palavrasChave = palavrasChave;
+        filtro(datFile, palavrasChave);
+    }
 
-	public void setDatFile(String datFile) {
-		this.datFile = datFile;
-	}
+    public DatFilter() {
+        super();
+    }
 
-	public String getPalavrasChave() {
-		return palavrasChave;
-	}
+    public String getDatFile() {
+        return datFile;
+    }
 
-	public void setPalavrasChave(String palavrasChave) {
-		this.palavrasChave = palavrasChave;
-	}
+    public void setDatFile(String datFile) {
+        this.datFile = datFile;
+    }
 
-	private boolean filtro(String datFile, String palavrasChave) {
+    public String getPalavrasChave() {
+        return palavrasChave;
+    }
 
-		Set<String> palavrasEncontradas = new HashSet<>(); 
+    public void setPalavrasChave(String palavrasChave) {
+        this.palavrasChave = palavrasChave;
+    }
+
+    private boolean filtro(String datFile, String palavrasChave) {
+
+        Set<String> palavrasEncontradas = new HashSet<>();
         List<String> listaPalavras = preencherListaPalavrasChave(palavrasChave);
         List<String> listaLinhasCompletas = new ArrayList<>();
-	
+
         try (BufferedReader reader = new BufferedReader(new FileReader(datFile))) {
-        	
+
             String linha;
-            String linhaAnterior = "";
-            
+
             if (listaPalavras.size() == 0 || listaPalavras == null) {
-            	return false;
+                return false;
             }
-            System.out.println(listaPalavras.toString());
             // Percorre o arquivo linha por linha
             while ((linha = reader.readLine()) != null) {
-    
-                for (String palavra: listaPalavras) {
-                	if (linha.toLowerCase().contains(palavra)) {
-                    	palavrasEncontradas.add(palavra);
+
+                for (String palavra : listaPalavras) {
+                    if (linha.toLowerCase().contains(palavra)) {
+                        palavrasEncontradas.add(palavra);
                     }
                 }
-                
+
                 if (palavrasEncontradas.containsAll(listaPalavras)) {
-                	listaLinhasCompletas.add(linha);
-                	System.out.println(linha);
+                    listaLinhasCompletas.add(linha);
+                    System.out.println(linha);
                 } else {
-                	System.out.println("Nem todas as palavras foram encontradas no arquivo.");
+                    System.out.println("Nem todas as palavras foram encontradas no arquivo.");
                 }
-                
-                linhaAnterior = linha;
+
                 palavrasEncontradas.clear();
             }
 
@@ -90,24 +86,24 @@ public class DatFilter {
             e.printStackTrace();
             return false;
         }
-        
-        return true;
-	}
 
-	private List<String> preencherListaPalavrasChave(String palavrasChaves) {
-		 String[] palavrasBusca = palavrasChaves.trim().toLowerCase().split(",");
-		 List<String> listaPalavras = Arrays.asList(palavrasBusca);
-		 String palavra = "";
-		 
-		 for (int i = 0; i < listaPalavras.size(); i++) {
-			 if (PALAVRAS_IGNORADAS.contains(listaPalavras.get(i))) {
-				 palavra = listaPalavras.get(i);
-				 System.out.println("Palavra removida: " + palavra);
-				 listaPalavras.remove(i);
-			 }
-			 System.out.println(listaPalavras.get(i));
-		 }
-		 
-		 return listaPalavras;
-	}
+        return true;
+    }
+
+    private List<String> preencherListaPalavrasChave(String palavrasChaves) {
+        String[] palavrasBusca = palavrasChaves.trim().toLowerCase().split(",");
+        List<String> listaPalavras = Arrays.asList(palavrasBusca);
+        String palavra = "";
+
+        for (int i = 0; i < listaPalavras.size(); i++) {
+            if (PALAVRAS_IGNORADAS.contains(listaPalavras.get(i))) {
+                palavra = listaPalavras.get(i);
+                System.out.println("Palavra removida: " + palavra);
+                listaPalavras.remove(i);
+            }
+            System.out.println(listaPalavras.get(i));
+        }
+
+        return listaPalavras;
+    }
 }
--- src/main/java/include/HtmlParser.java
+++ src/main/java/include/HtmlParser.java
@@ -4,7 +4,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.text.Normalizer;
+import java.util.regex.Pattern;
 
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -13,149 +14,159 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class HtmlParser {
-	private String inputHTMLFile;
-	private String outputDATFile;
-	private String inputFolder;
+  private String inputHTMLFile;
+  private String outputDATFile;
+  private String inputFolder;
 
-	public HtmlParser() {
-		super();
-	}
+  public HtmlParser() {
+    super();
+  }
 
-	public HtmlParser(String inputHTMLFile, String outputDATFile, String inputFolder) {
-		super();
-		this.inputHTMLFile = inputHTMLFile;
-		this.outputDATFile = outputDATFile;
-		this.inputFolder = inputFolder;
-	}
+  public HtmlParser(String inputHTMLFile, String outputDATFile, String inputFolder) {
+    super();
+    this.inputHTMLFile = inputHTMLFile;
+    this.outputDATFile = outputDATFile;
+    this.inputFolder = inputFolder;
+  }
 
-	public String getInputFolder() {
-		return inputFolder;
-	}
+  public String getInputFolder() {
+    return inputFolder;
+  }
 
-	public void setInputFolder(String inputFolder) {
-		this.inputFolder = inputFolder;
-	}
+  public void setInputFolder(String inputFolder) {
+    this.inputFolder = inputFolder;
+  }
 
-	public String getInputHTMLFile() {
-		return inputHTMLFile;
-	}
+  public String getInputHTMLFile() {
+    return inputHTMLFile;
+  }
 
-	public void setInputHTMLFile(String inputHTMLFile) {
-		this.inputHTMLFile = inputHTMLFile;
-	}
+  public void setInputHTMLFile(String inputHTMLFile) {
+    this.inputHTMLFile = inputHTMLFile;
+  }
 
-	public String getOutputDATFile() {
-		return outputDATFile;
-	}
+  public String getOutputDATFile() {
+    return outputDATFile;
+  }
 
-	public void setOutputDATFile(String outputDATFile) {
-		this.outputDATFile = outputDATFile;
-	}
+  public void setOutputDATFile(String outputDATFile) {
+    this.outputDATFile = outputDATFile;
+  }
 
-	public boolean parseFile(String outputDatFile) {
-		String inputedFile = "";
-		JFileChooser chooser = new JFileChooser();
+  public boolean parseFile(String outputDatFile) {
+    String inputedFile = "";
+    JFileChooser chooser = new JFileChooser();
 
-		// Filtro para o chooser
-		FileNameExtensionFilter filtro = new FileNameExtensionFilter("Selecione apenas HTML", "html");
+    // Filtro para o chooser
+    FileNameExtensionFilter filtro = new FileNameExtensionFilter("Selecione apenas HTML", "html");
 
-		// Coloca o filtro no chooser
-		chooser.setFileFilter(filtro);
+    // Coloca o filtro no chooser
+    chooser.setFileFilter(filtro);
 
-		int retorno = chooser.showOpenDialog(null);
+    int retorno = chooser.showOpenDialog(null);
 
-		if (retorno == JFileChooser.APPROVE_OPTION) {
-			inputedFile = chooser.getSelectedFile().getAbsolutePath();
-		} else {
-			return false;
-		}
+    if (retorno == JFileChooser.APPROVE_OPTION) {
+      inputedFile = chooser.getSelectedFile().getAbsolutePath();
+    } else {
+      return false;
+    }
 
-		try {
-			// O objeto File faz parte do java.io
-			File inputFile = new File(inputedFile);
-			// Transforma em UTF-8 para que seja possível a leitura de alguns caracteres da
-			// língua portuguesa
-			Document documentoHTML = Jsoup.parse(inputFile, "UTF-8");
-			String text = documentoHTML.text();
+    try {
+      // O objeto File faz parte do java.io
+      File inputFile = new File(inputedFile);
+      // Transforma em UTF-8 para que seja possível a leitura de alguns caracteres da
+      // língua portuguesa
+      Document documentoHTML = Jsoup.parse(inputFile, "UTF-8");
+      String text = documentoHTML.text();
 
-			try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputDatFile))) {
-				writer.newLine();
-				writer.write("[arquivo]: " + inputFile.getName());
-				writer.write(text);
-				System.out.println("Arquivo .dat criado com sucesso!");
-			} catch (IOException e) {
-				System.err.println("Erro ao criar o arquivo .dat" + e.getMessage());
-				return false;
-			}
-		} catch (IOException f) {
-			System.err.println("Erro ao processar o arquivo: " + f.getMessage());
-			return false;
-		}
-		return true;
-	}
+      try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputDatFile))) {
+        writer.newLine();
+        writer.write("[arquivo]: " + inputFile.getName());
+        writer.write(text);
+        System.out.println("Arquivo .dat criado com sucesso!");
+      } catch (IOException e) {
+        System.err.println("Erro ao criar o arquivo .dat" + e.getMessage());
+        return false;
+      }
+    } catch (IOException f) {
+      System.err.println("Erro ao processar o arquivo: " + f.getMessage());
+      return false;
+    }
+    return true;
+  }
 
-	public boolean parseDirectory(String inputDirectory, String outputDatFile) {
-		File pasta = new File(inputDirectory);
-		if (!pasta.exists() || !pasta.isDirectory()) {
-			System.err.println("O diretório especificado não existe ou não é um diretório.");
-			return false;
-		}
+  public boolean parseDirectory(String inputDirectory, String outputDatFile) {
+    File pasta = new File(inputDirectory);
+    if (!pasta.exists() || !pasta.isDirectory()) {
+      System.err.println("O diretório especificado não existe ou não é um diretório.");
+      return false;
+    }
 
-		File[] arquivosHtml = pasta.listFiles((dir, name) -> name.toLowerCase().endsWith(".html"));
-		if (arquivosHtml == null || arquivosHtml.length == 0) {
-			System.out.println("Nenhum arquivo HTML encontrado no diretório.");
-			return false;
-		}
+    File[] arquivosHtml = pasta.listFiles((dir, name) -> name.toLowerCase().endsWith(".html"));
+    if (arquivosHtml == null || arquivosHtml.length == 0) {
+      System.out.println("Nenhum arquivo HTML encontrado no diretório.");
+      return false;
+    }
 
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputDatFile, StandardCharsets.UTF_8))) {
-			// Processa cada arquivo HTML
-			for (File file : arquivosHtml) {
-				if (file.isFile()) {
-					try {
-						Document document = Jsoup.parse(file, "UTF-8");
-						String text = document.text();
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputDatFile))) {
+      int fileIndex = 1;
+      // Processa cada arquivo HTML
+      for (File file : arquivosHtml) {
+        if (file.isFile()) {
+          try {
+            Document document = Jsoup.parse(file, "UTF-8");
+            String text = document.text();
+            String textoPuro = removerAcentos(text);
+            // Escreve o nome do arquivo e o conteúdo no arquivo de saída
+            writer.newLine();
+            writer.write("[arquivo" + fileIndex + "]:" + file.getName());
+            writer.newLine();
+            writer.write(textoPuro);
+            fileIndex++;
+          } catch (IOException e) {
+            System.err.println("Erro ao processar o arquivo " + file.getName() + ": " + e.getMessage());
+            return false;
+          }
+        }
+      }
+      System.out.println("Todos os arquivos HTML foram processados e o conteúdo foi salvo em " + outputDatFile);
+    } catch (IOException e) {
+      System.err.println("Erro ao escrever no arquivo .dat: " + e.getMessage());
+      return false;
+    }
+    return true;
+  }
 
-						// Escreve o nome do arquivo e o conteúdo no arquivo de saída
-						writer.newLine();
-						writer.write("[arquivo]: " + file.getName());
-						writer.newLine();
-						writer.write(text);
-					} catch (IOException e) {
-						System.err.println("Erro ao processar o arquivo " + file.getName() + ": " + e.getMessage());
-						return false;
-					}
-				}
-			}
-			System.out.println("Todos os arquivos HTML foram processados e o conteúdo foi salvo em " + outputDatFile);
-		} catch (IOException e) {
-			System.err.println("Erro ao escrever no arquivo .dat: " + e.getMessage());
-			return false;
-		}
-		return true;
-	}
+  // public String extractMetadados(File inputFile) {
+  // StringBuilder metaInfo = new StringBuilder();
+  //
+  // try {
+  // Document document = Jsoup.parse(inputFile, "UTF-8");
+  //
+  // // Uma biblioteca do Jsoup feita para manipular os elementos presentes no
+  // HTML
+  // Elements metadados = document.getElementsByTag("meta");
+  //
+  // for (Element meta : metadados) {
+  // String name = meta.attr("name");
+  // String content = meta.attr("content");
+  //
+  // if (!name.isEmpty()) {
+  // metaInfo.append("Name: ").append(name).append("\n");
+  // metaInfo.append("Content: ").append(content).append("\n");
+  // }
+  //
+  // }
+  // } catch (IOException e) {
+  // metaInfo.append("Erro ao processar os metadados:
+  // ").append(e.getMessage()).append("\n");
+  // }
+  // return metaInfo.toString();
+  // }
 
-//	public String extractMetadados(File inputFile) {
-//		StringBuilder metaInfo = new StringBuilder();
-//
-//		try {
-//			Document document = Jsoup.parse(inputFile, "UTF-8");
-//
-//			// Uma biblioteca do Jsoup feita para manipular os elementos presentes no HTML
-//			Elements metadados = document.getElementsByTag("meta");
-//
-//			for (Element meta : metadados) {
-//				String name = meta.attr("name");
-//				String content = meta.attr("content");
-//
-//				if (!name.isEmpty()) {
-//					metaInfo.append("Name: ").append(name).append("\n");
-//					metaInfo.append("Content: ").append(content).append("\n");
-//				}
-//				
-//			}
-//		} catch (IOException e) {
-//			metaInfo.append("Erro ao processar os metadados: ").append(e.getMessage()).append("\n");
-//		}
-//		return metaInfo.toString();
-//	}
+  public String removerAcentos(String texto) {
+    String textoNormalizado = Normalizer.normalize(texto, Normalizer.Form.NFD);
+    Pattern padrao = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+    return padrao.matcher(textoNormalizado).replaceAll("").replaceAll("[^a-zA-Z0-9\\s]", "");
+  }
 }
